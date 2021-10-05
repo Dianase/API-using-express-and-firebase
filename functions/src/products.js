be925b227e158d17c33b857e7981dae149d5c9dc
Diana Tineo <diana.tineo1@gmail.com>
2021-10-05 16:47:54 -0400

    added additional endpoints

--- functions/src/products.js
+++ functions/src/products.js
@@ -40,3 +40,57 @@ exports.getProductById = (req, res) =>{
   //return document
 
 }
+
+exports.createProduct = (req, res) => {
+  //check that all required fields are present
+  if(!req.body.sku || !req.body.type || !req.body.price){
+    res.status(401).send({ message: 'Invalid Request'})
+    return //exits the function
+  }
+  //construct new clothing from .body
+  let newItem = {
+    sku: req.body.sku,
+    type: req.body.type,
+    price: Number(req.body.price.toFixed(2)),
+    graphic: (req.body.graphic) ? true : false,
+  }
+  if(req.body.sizes) newItem.sizes = req.body.sizes
+
+  if(req.body.brand) newItem.brand = req.body.brand
+
+  if(req.body.style) newItem.style = req.body.style
+
+  if(req.body.color) newItem.color = req.body.color
+
+  const db = connectDb()
+    db.collection('clothes').add(newItem)
+    .then(docRef => res.status(201).send({ id: docRef.id }))
+    .catch(err => res.status(201).send(err))
+  
+}
+
+exports.updateProduct = (req, res) => {
+  const { productId } = req.params
+  let newUpdates = {}
+  if(req.body.type) newUpdates.type = req.body.type
+  if(req.body.sizes) newUpdates.sizes = req.body.sizes
+  if(req.body.price) newUpdates.price = req.price = Number(req.body.price.toFixed(2))
+  if(req.body.brand) newUpdates.brand = req.body.brand
+  if(req.body.style) newUpdates.style = req.body.style
+  if(req.body.color) newUpdates.color = req.body.color
+  if(req.body.sku) newUpdates.sku = req.body.sku
+
+  
+  const db = connectDb()
+  db.collection('clothes').doc(productId).update(req.body)
+  .then(() => res.status(201).send({message: 'updated'}))
+  .catch(err => res.status(500).send(err))
+}
+
+exports.deleteProduct = (req, res) => {
+  const { productId } = req.params
+  const db = connectDb()
+  db.collection('clothes').doc(productId).delete()
+    .then(()=> res.status(202).send({message: 'deleted'}))
+    .catch(err => res.status(500).send(err))
+}
